0a5ee72846d6f039a267f842de599e60ebde9da0
lemon <wymcmh@sina.com>
2026-01-06 18:34:14 +0800

    更换gemini-live-2.5-flash-preview为gemini-2.5-flash-native-audio-preview-12-2025

--- src/static/js/config/config.js
+++ src/static/js/config/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     API: {
         VERSION: 'v1beta',
-        MODEL_NAME: 'models/gemini-live-2.5-flash-preview'
+        MODEL_NAME: 'models/gemini-2.5-flash-native-audio-preview-12-2025'
     },
     // You can change the system instruction to your liking
     SYSTEM_INSTRUCTION: {
